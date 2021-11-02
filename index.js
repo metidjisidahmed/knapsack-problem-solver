e2740be2c799712b22bcfd8245b39dc50be0b1e2
Metidji Sid Ahmed <is_metidji@esi.dz>
2021-11-02 12:08:10 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let objects =[
+    {},
+    {
+        weight : 2,
+        value : 5
+    },
+    {
+        weight : 1,
+        value :3
+    },
+    {
+        weight :4 ,
+        value :7
+    },
+    {
+        weight :3 ,
+        value :1
+    },
+    {
+        weight :5 ,
+        value :6
+    },
+];
+let nbObjects = objects.length;
+let maxCapacity = 10+1;
+let P = Array(nbObjects).fill().map(()=>Array(maxCapacity).fill(0));
+// console.log(P)
+for (let i = 1 ; i<= nbObjects-1 ; i++){
+    for(let j = 1 ; j<= maxCapacity-1 ; j++){
+        console.log(' I =', i, 'J = ', j);
+        let wi=objects[i].weight;
+        let vi = objects[i].value;
+        if( j < wi ){
+            P[i][j] = P[i - 1][j];
+        }else{
+            P[i][j] = Math.max(P[i - 1][j], P[i - 1][j - wi] + vi);
+        }
+    }
+}
+
+console.log(P);
