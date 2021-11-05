75a43a10d9bb70c0f86e137630f1d93d42b6a33a
Metidji Sid Ahmed <is_metidji@esi.dz>
2021-11-05 12:19:14 +0100

    V1.0 ( just need tofix the index of the maximum profit )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { Paper } from "@material-ui/core";
 import MaterialTable , {MTableHeader} from "material-table";
 import { tableIcons, tableLang } from './materialTableOptions/index';
 import { makeStyles } from '@material-ui/core/styles';
 import KnapsackTable from "./components/KnapsackTable";
 import {solver , objects , maxCapacity} from './noyau/noyau';
-import { useState} from 'react'
+import { useState} from 'react';
+import {Button, TextField , Paper , Box , AppBar , Toolbar , IconButton , Typography} from "@mui/material";
+import {Delete} from "@material-ui/icons";
+import {Calculate, Replay , Menu  } from "@mui/icons-material";
+import React from "react";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -51,6 +54,7 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
     const classes = useStyles();
+    const [W, setW] = useState(0);
     const [objects, setObjects] = useState([
         {
             weight : 2,
@@ -73,6 +77,7 @@ function App() {
             value :6
         },
     ]);
+    const [resultatMode, setResultatMode] = useState(false);
     /* default table options */
     const tableOptions = {
         sorting : true,
@@ -81,70 +86,113 @@ function App() {
 
     /* default table structure */
     const columns = [
-        { field: 'weight', title: "Poids" , render: rowData => (
+        { field: 'weight', title: "Poids ( Wi )" , render: rowData => (
         <div className="text-center" style={{fontSize : '0.8rem'}}>{rowData.weight}</div>
     )  },
-        { field: 'value', title: "Profit" , render: rowData => (
+        { field: 'value', title: "Profit ( Vi )" , render: rowData => (
                 <div className="text-center" style={{fontSize : '0.8rem'}}>{rowData.value}</div>
             ) },
     ];
+    const Header =()=>{
+        return (
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            KnapSack Problem Website solver ,  <b>By Metidji Sid Ahmed & Gouaouri Mohamed Dhiaa Eddine</b>
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            )
 
+
+    }
     return (
-    <div className="App pe-5 ps-5 mt-4 mb-4">
-      <Paper variant="elevation" elevation={8} className={classes.card_paper}  >
-        <MaterialTable
-            components={{
-                Header: props => (
-                       <MTableHeader {...props}  style={{textAlign : 'center'}}/>
-                )
-            }}
-            title="Introduire les objets disponibles ici : "
-            // onRowClick={(event, rowData, toggleDetailPanel)=>{dispatch(copyAndsetSnackBarContent(rowData.riderPhoneNumber))}}
-            icons={tableIcons}
-            options={tableOptions}
-            localization={tableLang}
-            columns={columns}
-            data={objects}
-            editable={
-                {
-                    onRowAdd : newData=>new Promise((resolve , reject)=>{
-                        if(newData.weight && newData.value){
-                            setObjects(oldState => oldState.concat(newData));
-                            resolve();
-                        }else{
-                            reject();
-                        }
-                    }),
-                    onRowUpdate: (newData, oldData) => {
-                        return new Promise((resolve , reject)=>{
-                            let actualData = objects.filter((obj, index) => {
-                                return (obj.weight === oldData.weight && obj.value === oldData.value)
+    <React.Fragment>
+        <Header/>
+        <div className="App pe-5 ps-5 mt-4 mb-4">
+            <div className="col-10 offset-1 pt-2 pb-2">
+                <TextField disabled={resultatMode} value={W} onChange={(e)=>setW(e.target.value)} error={W <= 0} helperText={'Le poids maximum doit avoit une valeur strictement positive !'} type={'number'} className={'w-100'}   label="Spécifiez le poids maximum de votre sac à dos ( W ) :" variant="outlined" />
+            </div>
+            <Paper variant="elevation" elevation={8} className={classes.card_paper}  >
+                <MaterialTable
+                    components={{
+                        Header: props => (
+                            <MTableHeader {...props}  style={{textAlign : 'center'}}/>
+                        )
+                    }}
+                    title="Introduire les objets disponibles ici : "
+                    // onRowClick={(event, rowData, toggleDetailPanel)=>{dispatch(copyAndsetSnackBarContent(rowData.riderPhoneNumber))}}
+                    icons={tableIcons}
+                    options={tableOptions}
+                    localization={tableLang}
+                    columns={columns}
+                    data={objects}
+                    editable={ !resultatMode ?
+                        {
+                            onRowAdd : newData=>new Promise((resolve , reject)=>{
+                                if(newData.weight && newData.value){
+                                    setObjects(oldState => oldState.concat(newData));
+                                    resolve();
+                                }else{
+                                    reject();
+                                }
+                            }),
+                            onRowUpdate: (newData, oldData) => {
+                                return new Promise((resolve , reject)=>{
+                                    let actualData = objects.filter((obj, index) => {
+                                        return (obj.weight === oldData.weight && obj.value === oldData.value)
 
-                            })[0];
-                            let actualIndex = objects.indexOf(actualData);
-                            console.log('Actual index =', actualIndex , 'Actual Data =', actualData);
-                            let newObjects = [...objects];
-                            newObjects[oldData.tableData.id] = newData;
-                            setObjects(newObjects);
-                            resolve();
-                        })
-                    },
-                    onRowDelete : oldData => {
-                        return new Promise((resolve , reject)=>{
-                            let newObjects = [...objects];
-                            newObjects.splice(1, 1);
-                            setObjects(newObjects);
-                            resolve();
-                        })
+                                    })[0];
+                                    let actualIndex = objects.indexOf(actualData);
+                                    console.log('Actual index =', actualIndex , 'Actual Data =', actualData);
+                                    let newObjects = [...objects];
+                                    newObjects[oldData.tableData.id] = newData;
+                                    setObjects(newObjects);
+                                    resolve();
+                                })
+                            },
+                            onRowDelete : oldData => {
+                                return new Promise((resolve , reject)=>{
+                                    let newObjects = [...objects];
+                                    newObjects.splice(1, 1);
+                                    setObjects(newObjects);
+                                    resolve();
+                                })
+                            }
+                        } : false
+
                     }
-                }
-
+                />
+            </Paper>
+            <hr/>
+            {
+                !resultatMode ? (
+                    <div className="col-12 justify-content-center d-flex mb-3">
+                        <Button disabled={W <= 0 || !objects.length} onClick={()=>setResultatMode(true)} style={ !(W <= 0 || !objects.length ) ?  {color : '#ffdd00' , borderColor : '#ffdd00'} : {}} variant="outlined" startIcon={<Calculate />}>
+                            Calculer le résultat
+                        </Button>
+                    </div>
+                ) : (
+                    <div className="col-12 justify-content-center d-flex mb-3">
+                        <Button disabled={W <= 0 || !objects.length} onClick={()=>setResultatMode(false)}  color="secondary" variant="outlined" startIcon={<Replay />}>
+                            Réessayer
+                        </Button>
+                    </div>
+                )
             }
-        />
-      </Paper>
-        <hr/>
-        <KnapsackTable maxCapacity={maxCapacity} P={solver(objects , 10+1)} nbObjects={objects.length}/>
-    </div>
+
+            {
+                resultatMode ? (
+                    <KnapsackTable maxCapacity={Number(W)} P={solver( [{} , ...objects] , Number(W)+1)} nbObjects={objects.length}/>
+                ) : null
+            }
+
+
+        </div>
+
+    </React.Fragment>
   );
 }
 
--- src/noyau/noyau.js
+++ src/noyau/noyau.js
@@ -28,8 +28,8 @@ export  function solver(objects , maxCapacity){
     for (let i = 1 ; i<= nbObjects-1 ; i++){
         for(let j = 1 ; j<= maxCapacity-1 ; j++){
             console.log(' I =', i, 'J = ', j);
-            let wi=objects[i].weight;
-            let vi = objects[i].value;
+            let wi=Number(objects[i].weight);
+            let vi = Number(objects[i].value);
             if( j < wi ){
                 P[i][j] = P[i - 1][j];
             }else{
